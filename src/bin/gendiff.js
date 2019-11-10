4d5117ff93a1077b2d4eeb670c3991228647ccd0
Dmitry <d.smirnoff.73@gmail.com>
2019-11-10 21:26:56 +0100

    3rd checkpoint

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
-import execute from '..';
+import commander from 'commander';
+import genDiff from '..';
 
-execute(process.argv);
+new commander.Command()
+    .version('1.0.2')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'Output format')
+    .arguments('<firstConfig>  <secondConfig>')
+    .action((firstConfig, secondConfig) => {
+      try {
+        const difference = genDiff(firstConfig, secondConfig);
+        console.log(difference);
+      } catch (error) {
+        console.log(error.toString());
+      }
+    })
+    .parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,41 @@
-import commander from 'commander';
+import fs from 'fs';
+import { resolve } from 'path';
+import { has } from 'lodash';
 
-export default (args) => {
-  const program = new commander.Command();
-  program
-    .version('1.0.2')
-    .description('Compares two configuration files and shows a difference.')
-    .parse(args);
+const tags = ['before', 'after'];
+
+const readAndParseFile = (pathToFile) => {
+  const fileContent = fs.readFileSync(resolve(pathToFile), 'utf-8');
+  return JSON.parse(fileContent);
+};
+
+const mapParsedFile = (map, parsedFile, fileNumber) => {
+  const newMap = Object.entries(parsedFile)
+    .reduce((_map, [key, value]) => {
+      const position = tags[fileNumber];
+      const newValue = has(_map, key)
+        ? { ..._map[key], [position]: value }
+        : { [position]: value };
+      return { ..._map, [key]: newValue};
+    },
+    map);
+  return newMap;
+};
+
+const mapToString = (differences) => {
+  const result = Object.entries(differences)
+    .map(([key, value]) => {
+      if (value.after === undefined) return `- ${key}: ${value.before}`;
+      if (value.before === undefined) return `+ ${key}: ${value.after}`;
+      if (value.before === value.after) return `  ${key}: ${value.before}`;
+      return `- ${key}: ${value.before}\n\t+ ${key}: ${value.after}`;
+    });
+  return `{\n\t${result.join('\n\t')}\n}`;
+}
+
+export default (...pathToFile) => {
+  const resultMap = pathToFile
+    .map(readAndParseFile)
+    .reduce(mapParsedFile, {});
+  return mapToString(resultMap);
 };
